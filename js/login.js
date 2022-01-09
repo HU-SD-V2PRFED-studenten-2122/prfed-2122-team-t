cdff515562cf02a92293bafccb4c732545083874
Samy <samyboy792@gmail.com>
2022-01-09 13:12:51 +0100

    login en logout

--- js/login.js
+++ js/login.js
@@ -20,12 +20,12 @@ class loginComponent extends LitElement{
 
             <form class="loginform">
                 <div class="form-outline mb-4">
-                    <label class="form-label" for="typeEmailX-2">Emailadres:</label>
+                    <label for="mailInput" class="form-label" for="typeEmailX-2">Emailadres:</label>
                     <input id="mailInput" type="email" class="form-control form-control-lg" placeholder="test@hu.nl" tabindex="0"/>
                 </div>
 
                 <div class="form-outline mb-4">
-                    <label class="form-label" for="typePasswordX-2">Wachtwoord:</label>
+                    <label for="wwInput" class="form-label" for="typePasswordX-2">Wachtwoord:</label>
                     <input id="wwInput" type="password" class="form-control form-control-lg" placeholder="••••••••••" tabindex="0" />
                 </div>
 
@@ -34,36 +34,37 @@ class loginComponent extends LitElement{
                 <a href="/index.html" class="btn btn-primary float-right" style="margin-right: 10px;" tabindex="0">Terug</a>   
             </form>
 
-        </div>    
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        </div>     
         `
     }    
 
+    createRenderRoot(){
+        return this;
+    }
+
     login() {
-        sessionStorage.setItem('email', this.shadowRoot.querySelector("#mailInput").value);
-        sessionStorage.setItem('wachtwoord',this.shadowRoot.querySelector("#wwInput").value);
+        sessionStorage.setItem('email', this.renderRoot.querySelector("#mailInput").value);
+        sessionStorage.setItem('wachtwoord',this.renderRoot.querySelector("#wwInput").value);
 
         let email = sessionStorage.getItem('email');
         let wachtwoord = sessionStorage.getItem('wachtwoord');
     
-        let emailInput = this.shadowRoot.querySelector("#mailInput");
-        let wachtwoordInput = this.shadowRoot.querySelector("#wwInput");
+        let emailInput = this.renderRoot.querySelector("#mailInput");
+        let wachtwoordInput = this.renderRoot.querySelector("#wwInput");
 
         var emails = storage.getGebruikers().map(gebruiker => gebruiker.email)        
         
             if (emailInput.value == '') {
-                this.shadowRoot.querySelector("#logintext").textContent = 'Email is leeg.'
-                this.shadowRoot.querySelector("#logintext").style.color = 'red';
+                this.renderRoot.querySelector("#logintext").textContent = 'Email is leeg.'
+                this.renderRoot.querySelector("#logintext").style.color = 'red';
             }
             if (wachtwoordInput.value == '') {
-                this.shadowRoot.querySelector("#logintext").textContent = 'Wachtwoord is leeg.'
-                this.shadowRoot.querySelector("#logintext").style.color = 'red';
+                this.renderRoot.querySelector("#logintext").textContent = 'Wachtwoord is leeg.'
+                this.renderRoot.querySelector("#logintext").style.color = 'red';
             }
             if (wachtwoordInput.value == '' && emailInput.value == '') {
-                this.shadowRoot.querySelector("#logintext").textContent = 'Email en wachtwoord zijn leeg.'
-                this.shadowRoot.querySelector("#logintext").style.color = 'red';
+                this.renderRoot.querySelector("#logintext").textContent = 'Email en wachtwoord zijn leeg.'
+                this.renderRoot.querySelector("#logintext").style.color = 'red';
             }
 
             if(emailInput.value != '' && wachtwoordInput.value != '') {
@@ -71,22 +72,22 @@ class loginComponent extends LitElement{
                     storage.getGebruikers().filter(gebruiker => {  
                         if(gebruiker.email == email && gebruiker.wachtwoord == wachtwoord ){
                             console.log(gebruiker)
-                            this.shadowRoot.querySelector("#logintext").textContent = 'Succesvol ingelogd!';
-                            this.shadowRoot.querySelector("#logintext").style.color = 'green';
+                            this.renderRoot.querySelector("#logintext").textContent = 'Succesvol ingelogd!';
+                            this.renderRoot.querySelector("#logintext").style.color = 'green';
                             sessionStorage.setItem("ingelogd","ja")
                             window.location = '/testpage.html'
                         }     
                         if(gebruiker.email == email && gebruiker.wachtwoord != wachtwoord ){
                             console.log(gebruiker)
-                            this.shadowRoot.querySelector("#logintext").textContent = 'Inloggegevens kloppen niet!';
-                            this.shadowRoot.querySelector("#logintext").style.color = 'red';
+                            this.renderRoot.querySelector("#logintext").textContent = 'Inloggegevens kloppen niet!';
+                            this.renderRoot.querySelector("#logintext").style.color = 'red';
                         }             
                       
                     })
                 }
                 else{
-                    this.shadowRoot.querySelector("#logintext").textContent = 'Account bestaat niet!';
-                    this.shadowRoot.querySelector("#logintext").style.color = 'red';
+                    this.renderRoot.querySelector("#logintext").textContent = 'Account bestaat niet!';
+                    this.renderRoot.querySelector("#logintext").style.color = 'red';
                 }
                
             }        
